0b4843f14c4e1358d624e3d11091be7c4dd482a7
PatosZapatos <131910318+PatosZapatos@users.noreply.github.com>
2025-08-26 21:28:27 -0300

    Add files via upload

--- src/ejercicio1/MiHilo.java
+++ src/ejercicio1/MiHilo.java
@@ -1,5 +1,11 @@
 package ejercicio1;
 
+//Pregunta para pensar: ¿Qué sucede si llamas a hilo.run() en lugar de hilo.start()? ¿Cuál es la diferencia?
+/* Llamar run() ejecuta el código de forma directa, no crea un nuevo hilo. 
+ * Llamar start() crea un nuevo hilo, y luego invoca run() en ese hilo.
+* */
+
+
 public class MiHilo extends Thread {
 
 	public void run() {
